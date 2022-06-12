0cfb4001c3ac85e2c22e16aafec315437aa9c482
chacheta <starkstory6@gmail.com>
2022-06-12 02:51:33 -0300

    Adicionado o listarFunc

---
+++ View/Funcionario/ListarFunc.php
@@ -0,0 +1,228 @@
+<?php
+
+// Conexao com o banco de dados:
+include_once("../../Banco/Conexao.php");
+
+//Iniciar a sessao
+session_start();
+
+//Limpar o buffer de saida
+ ob_start();
+
+//verifica se a sessão usuario existe  
+require_once('includes/sessao.php');
+
+//inclui a foto do usuário
+include_once "includes/foto.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
+    <title>Cadastrar Funcionario</title>
+
+
+    <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/modules/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="assets/modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+
+  <link rel="stylesheet" href="assets/css/demo.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <style>
+      * label{
+          color:black;
+      }
+
+  </style>
+</head>
+
+<body>
+    <div id="app">
+        <div class="main-wrapper">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+                <form class="form-inline mr-auto">
+                    <ul class="navbar-nav mr-3">
+                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="ion ion-navicon-round"></i></a></li>
+                    </ul>
+                </form>
+                <ul class="navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg">
+                            <i class="ion ion-android-person d-lg-none"></i>
+                            <div class="d-sm-none d-lg-inline-block">olá, <?php echo $_SESSION['usuario']?></div>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a href="profile.html" class="dropdown-item has-icon">
+                                <i class="ion ion-android-person"></i> Perfil
+                            </a>
+                            <a href="../../Controller/Funcionario/sair.php" class="dropdown-item has-icon">
+                                <i class="ion ion-log-out"></i> Sair
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+            <div class="main-sidebar">
+                <aside id="sidebar-wrapper">
+                    <div class="sidebar-brand">
+                        <a href="index.html">Grant-Food</a>
+                    </div>
+                    <div class="sidebar-user">
+                        <div class="sidebar-user-picture">
+                        <?php
+                            if (!is_null($foto)){ ?>
+                            <img  class="img d-flex align-items-center justify-content-center" src="assets/img/FotoPerfil/<?php echo $foto ?>" alt="" style="width:75px;height: 75px;">
+                            <?php }else{ ?>
+                                <img  class="img d-flex align-items-center justify-content-center" src="assets/img/fundo.jpg" alt="" style="width:105px;height: 75px;">
+                            <?php }?>
+                        </div>
+                        <div class="sidebar-user-details">
+                            <div class="user-name"><?php echo $_SESSION['usuario'];?></div>
+                            <div class="user-role">
+                                Gerente
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="sidebar-menu">
+
+                        <li class="menu-header">Opções</li>
+                        <li>
+                            <a href="statusMesa.php"><i class="ion ion-clipboard"></i><span>Status da Mesa</span></a>
+                        </li>
+                        <li class="active">
+                            <a href="#" class="has-dropdown"><i class="ion ion-ios-people"></i><span>Funcionários</span></a>
+                            <ul class="menu-dropdown">
+                                <li><a href="CadastrarFunc.php"><i class="ion ion-person-add"></i>Cadastrar Funcionário</a></li>
+                                <li class="active"><a href="listarFunc.php"><i class="ion ion-ios-eye"></i>Consultar Funcionário</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class="has-dropdown"><i class="ion ion-ios-cart"></i><span>Cardápio</span></a>
+                            <ul class="menu-dropdown">
+                                <li><a href="ion-icons.html"><i class="ion ion-pizza"></i>Cadastrar Produto</a></li>
+                                <li><a href="fontawesome.html"><i class="ion ion-ios-eye"></i>Consultar Produto</a></li>
+                                <li><a href="flag.html"><i class="ion ion-ios-eye"></i>Consultar Categoria</a></li>
+                            </ul>
+                        </li>
+                        <li >
+                            <a href="simple.html"><i class="ion ion-medkit"></i><span>Inserir</span></a>
+                        </li>
+                        <li >
+                            <a href="simple.html"><i class="ion ion-clipboard"></i><span>Relatorio de vendas</span></a>
+                        </li>
+                </aside>
+            </div>
+            <div class="main-content">
+                <section class="section">
+                    <h1 class="section-header">
+                        <div>Funcionários cadastrados no sistema</div>
+                    </h1>
+
+        <!-- Pagina principal -->
+     <form method="POST">
+            <div>
+              <?php
+                if (isset($_SESSION['msg'])) {
+                    echo $_SESSION['msg'];
+                    unset($_SESSION['msg']);
+                }
+              ?>
+            </div>
+              <div>
+              <?php $sql = "SELECT * FROM usuario"; $result = $connection->query($sql);?>
+              <table class="table alert alert-primary">
+                <thead>
+ 
+                    <tr>
+                    <th scope="col" style="color:black">Id</th>
+                    <th scope="col" style="color:black">Nome</th>
+                    <th scope="col" style="color:black">Usuário</th>
+                    <th scope="col" style="color:black">Salário</th>
+                    <th scope="col" style="color:black">Permissão</th>
+                    <th scope="col" style="color:black">Ações</th>
+                    </tr>
+                </thead>
+                <?php if ($result->num_rows > 0) { while($row = $result->fetch_assoc()) {?> 
+                <tbody>
+                    <tr>
+                    <th scope="row"><?php echo $row["idFunc"]; ?></th>
+                    <td><?php echo $row["nome"]; ?></td>
+                    <td><?php echo $row["usuario"]; ?></td>
+                    <td><?php echo $row["salario"]; ?></td>
+                    <?php
+                    
+                      switch($row['tipo']){
+                        case 1:
+                          $tipo = 'Gerente';
+                          break;
+                        case 2:
+                          $tipo = "Garçom";
+                          break;
+                        case 3:
+                          $tipo = "Cozinha";
+                          break;
+                      }
+                    ?>
+                    <td><?php echo $tipo;?></td>
+                    <td> 
+                      <a href="editarFunc.php?id=<?php echo $row['idFunc']; ?>"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg>
+                      </a> 
+                      <a href="../../Model/Funcionario/excluirFunc.php?id=<?php echo  $row['idFunc']; ?>"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>
+                      </a> 
+                    </td> 
+                    </tr>
+                </tbody>
+                <?php   }}else{echo '<div class="alert alert-danger" role="alert">
+                                        &#128552 nenhum usuário cadastrado!
+                                      </div>';} ?> 
+                </table>
+              </div>
+            </form>
+          </div>  
+        </div>
+      </div>
+
+    </section>
+            </div>
+            <footer class="main-footer">
+                <div class="footer-left" style="color:black;">
+                    COPYRIGHT &copy; 2022
+                    <div class="bullet"></div> Todos os direitos reservados a Gran-Food</a>
+                </div>
+                <div class="footer-right"></div>
+            </footer>
+        </div>
+    </div>
+
+
+    <script src="assets/modules/jquery.min.js"></script>
+  <script src="assets/modules/popper.js"></script>
+  <script src="assets/modules/tooltip.js"></script>
+  <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="assets/modules/scroll-up-bar/dist/scroll-up-bar.min.js"></script>
+  <script src="assets/js/sa-functions.js"></script>
+  
+  <script src="http://maps.google.com/maps/api/js?key=YOUR_API_KEY&amp;sensor=true"></script>
+  <script src="assets/modules/gmaps.js"></script>
+  <script>
+    // init map
+    var simple_map = new GMaps({
+      div: '#simple-map',
+      lat: -6.5637928,
+      lng: 106.7535061
+    })
+  </script>
+  <script src="assets/js/scripts.js"></script>
+  <script src="assets/js/custom.js"></script>
+  <script src="assets/js/demo.js"></script>
+  <script src="assets/js/cepFunc.js"></script>
+</body>
+
+</html>
